50b7fe1ba5e7365a655b702b0b6a7b37ec5a8c9c
Emma Parsley <40206111@live.napier.ac.uk>
2019-05-10 16:55:31 +0100

    Update Maths.h
    -Added squared length template
    -Added notmalize template

--- engine/Code/Maths.h
+++ engine/Code/Maths.h
@@ -10,9 +10,29 @@ namespace math
 	//return the length of a vector
 	template <typename T> double length(const sf::Vector2<T> &v)
 	{
-		return sqrt(v.x * v.x + v.y + v.y);
+		return sqrt(length2(v));
 	}
 
+	//return the squared length of a vector
+	template <typename T> double length2(const sf::Vector2<T> &v)
+	{
+		return v.x * v.x + v.y * v.y;
+	}
 
+	//Normalize vector
+	template <typename T> sf::Vector2<T> normalize(const sf::Vector2<T> &v)
+	{
+		sf::Vector2 vector;
+		double l = length(v);	//find length
 
+		if (l != 0)	//avoid dividing by 0
+		{
+			v.x /= l;
+			v.y /= l;
+		}
+
+		return vector;
+	}
+
+	//
 }
